07c9a9d1df054961dcff51c7bc1ea80252e81bf0
Durgesh Singh <durgeshsingh18101999@gmail.com>
2024-05-17 20:31:50 +0530

    hosting project

--- server/index.js
+++ server/index.js
@@ -1,28 +1,34 @@
 import express from "express";
 import mongoose from "mongoose";
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 import route from "./routes/userRoute.js";
+import path from "path";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config();
 
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 7000;
 const URL = process.env.MONGOURL;
 
-mongoose.connect(URL).then(()=>{
-
+mongoose
+  .connect(URL)
+  .then(() => {
     console.log("DB connected successfully");
 
-    app.listen(PORT, ()=>{
-        console.log(`Server is running on port: ${PORT}`);
-    })
-
-}).catch(error => console.log(error));
-
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
 
 app.use("/api", route);
